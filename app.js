9f01ef61858c0fd78f2859cec58d1b2ce1537331
febanda01 <febanda91@gmail.com>
2019-07-17 00:17:37 -0500

    connected-to-DB

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ const express = require('express');
 //Execute it 
 const app = express();
 
+const mongoose = require('mongoose')
+
+
+//Middlewares - function that executes when routes are being hit 
+// app.use('/posts', () => {
+//     console.log('This is middleware running')
+// })
+
 
 //ROUTES 
 app.get('/', (req, res) => {
     res.send('We are on home')
+})
+
+app.get('/posts', (req, res) => {
+    res.send('We are on posts')
+})
+
+//Connect to DB 
+mongoose.connect('mongodb+srv://febanda91:Frankie@1991@cluster0-l1rbz.mongodb.net/test?retryWrites=true&w=majority',
+{ useNewUrlParser: true },
+() => {console.log('connected to DB!')
 })
 
 
